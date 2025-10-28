64ed570239e9645e16c4757e56096329366a0079
renaneliakim <renaneliakim1@gmail.com>
2025-10-28 12:30:03 -0300

    fix: docker

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!0$7hljbrd1+olf1i9^nl3r2#fcrna((=3)e^474d-+j&npax-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.89', '*']
 
 
 # Application definition
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
